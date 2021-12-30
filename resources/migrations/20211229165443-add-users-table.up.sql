fa158d36dd183ba2e6d875993bb66f33e62b7e70
Laurence Chen <humorless@gmail.com>
2021-12-30 13:11:51 +0800

    change shema + add telephone + change id generation

--- resources/migrations/20211229165443-add-users-table.up.sql
+++ resources/migrations/20211229165443-add-users-table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE students
 (id serial PRIMARY KEY,
  student_name text NOT NULL,
  student_birthday DATE NOT NULL,
- parent_name text NOT NULL,
+ telephone text NOT NULL,
  kuops_id text UNIQUE NOT NULL);
--- src/clj/kuops/domain/id.clj
+++ src/clj/kuops/domain/id.clj
@@ -1,36 +1,10 @@
-(ns kuops.domain.id
-  (:require [buddy.core.codecs :as codec]
-            [clojure.string :as str]))
+(ns kuops.domain.id)
 
-(defn hexify [s]
-  (-> s
-      codec/str->bytes
-      codec/bytes->hex))
-
-(defn unhexify [hex]
-  (-> hex
-      codec/hex->bytes
-      codec/bytes->str))
-
-(defn generate
-  [{:keys [s-name birthday-str]}]
-  (let [name-hex (hexify s-name)
-        day-hex   (hexify birthday-str)]
-    (str name-hex "-" day-hex)))
-
-(defn parse
-  [id-str]
-  (let [[name-hex day-hex] (str/split id-str #"-")
-        name-s (unhexify name-hex)
-        day (unhexify day-hex)]
-    {:name name-s
-     :birthday day}))
+(defn generate 
+  [{:keys [classroom serial]}]
+  (str classroom "_" serial))
 
 (comment
-  (hexify "楊")
-  (hexify "楊過")
-  (unhexify "e6a58a")
-  (unhexify "e6a58ae9818e")
   (generate
-   {:s-name "楊過"
-    :birthday-str "19871212"}))
+   {:classroom "AABBCC"
+    :serial 12344}))
